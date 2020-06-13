be2e7bd6da43c2e51a1e0ff159f50d46cf2943c9
kioie <kioi@outlook.com>
2020-06-13 10:51:04 +0300

    Test functions on live API.

--- main/main.go
+++ main/main.go
@@ -6,19 +6,22 @@ import (
 )
 
 func main() {
-	projectID := "secret-manager-test"
+	gcp_secret_manager.ProjectId = "secret-manager-test"
+	//projectID := "secret-manager-test"
 	//connect :=&gcp_secret_manager.NewClient{}
 	//	if !gcp_secret_manager.SecretExists("projects/1092054168008/secrets/my-secret") {
 	//		fmt.Println("Secret does not Exists")
 	//	}
 
 	//	fmt.Print(gcp_secret_manager.CreateSecret(projectID, []byte("my secret data")))
-	//	result := gcp_secret_manager.GetSecret("my-secret", "", projectID)
-	//	fmt.Println(result.Payload)
+	//result := gcp_secret_manager.GetSecret("my-secret", "", projectID)
+	//fmt.Println(result)
 	//	newResult := gcp_secret_manager.AddNewSecretVersion("my-secret", projectID, []byte("a new test"))
 	//	fmt.Println(newResult)
-	//	result2 := gcp_secret_manager.GetSecret("my-secret", "1", projectID)
-	//fmt.Println(result2.Payload)
-	result3 := gcp_secret_manager.GetSecretMetadata("my-secret", "1", projectID)
-	fmt.Println(result3.GetName())
+	result2 := gcp_secret_manager.GetSecret("my-secret", "")
+	fmt.Println(result2)
+	//	result3 := gcp_secret_manager.GetSecretMetadata("my-secret", "1", projectID)
+	//	fmt.Println(result3.GetName())
+
+	//fmt.Println(gcp_secret_manager.SecretExists(projectID,"my-secret"))
 }
