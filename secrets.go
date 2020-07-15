2e47688c32c8b6e0a4fb9a93e610f08becf554fe
kioie <kioi@outlook.com>
2020-07-15 08:35:32 +0300

    Change handling of global variables on secrets.go & create new client

--- secrets.go
+++ secrets.go
@@ -27,92 +27,93 @@
 package gcpsecretmanager
 
 import (
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"context"
 	"fmt"
-	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 	"log"
+	
+	sm "cloud.google.com/go/secretmanager/apiv1"
+	pb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
 
-var (
-	// Api Secret Client
-	client SecretClient
-	// ProjectID that is used as a global var
-	ProjectID string
-)
-var ctx = context.Background()
-var c, _ = secretmanager.NewClient(ctx)
+// Client is a global exported Client struct
+type Client struct {
+	smc SecretClient
+}
 
-func init() {
-
-	client = &secretClientImpl{client: c}
-
+// NewClient is a global exported function that creates a new client
+func NewClient(ctx context.Context) (*sm.Client, error) {
+	client, err := sm.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
 
 // CreateEmptySecret function
-func CreateEmptySecret(secretName string) (*secretmanagerpb.Secret, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	createSecretReq := &secretmanagerpb.CreateSecretRequest{
-		Parent:   fmt.Sprintf("projects/%s", ProjectID),
+func (c *Client) CreateEmptySecret(ctx context.Context, secretName string, projectId string) (*pb.Secret, error) {
+	createSecretReq := pb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%s", projectId),
 		SecretId: secretName,
-		Secret: &secretmanagerpb.Secret{
-			Replication: &secretmanagerpb.Replication{
-				Replication: &secretmanagerpb.Replication_Automatic_{
-					Automatic: &secretmanagerpb.Replication_Automatic{},
+		Secret: &pb.Secret{
+			Replication: &pb.Replication{
+				Replication: &pb.Replication_Automatic_{
+					Automatic: &pb.Replication_Automatic{},
 				},
 			},
 		},
 	}
-	secret, err := client.CreateSecret(ctx,createSecretReq)
+	
+	secret, err := c.smc.CreateSecret(ctx, &createSecretReq)
 	if err != nil {
 		log.Printf("failed to create secret: %v", err)
 		return nil, err
 	}
+	
 	return secret, nil
 }
 
 // CreateSecretWithData creates secret with data
-func CreateSecretWithData(secretName string, payload []byte) (*secretmanagerpb.SecretVersion, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	createSecretReq := &secretmanagerpb.CreateSecretRequest{
-		Parent:   fmt.Sprintf("projects/%s", ProjectID),
+func (c *Client) CreateSecretWithData(ctx context.Context, secretName string, payload []byte, projectId string) (*pb.SecretVersion, error) {
+	createSecretReq := pb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%s", projectId),
 		SecretId: secretName,
-		Secret: &secretmanagerpb.Secret{
-			Replication: &secretmanagerpb.Replication{
-				Replication: &secretmanagerpb.Replication_Automatic_{
-					Automatic: &secretmanagerpb.Replication_Automatic{},
+		Secret: &pb.Secret{
+			Replication: &pb.Replication{
+				Replication: &pb.Replication_Automatic_{
+					Automatic: &pb.Replication_Automatic{},
 				},
 			},
 		},
 	}
-	secret, err := client.CreateSecret(ctx,createSecretReq)
+	
+	secret, err := c.smc.CreateSecret(ctx, &createSecretReq)
 	if err != nil {
 		log.Printf("failed to create secret: %v\n", err)
 		return nil, err
 	}
-	addSecretVersionReq := &secretmanagerpb.AddSecretVersionRequest{
+	
+	addSecretVersionReq := pb.AddSecretVersionRequest{
 		Parent: secret.Name,
-		Payload: &secretmanagerpb.SecretPayload{
+		Payload: &pb.SecretPayload{
 			Data: payload,
 		},
 	}
-	version, err := client.AddSecretVersion(ctx, addSecretVersionReq)
+	
+	version, err := c.smc.AddSecretVersion(ctx, &addSecretVersionReq)
 	if err != nil {
 		log.Printf("failed to add secret version: %v\n", err)
 		return nil, err
 	}
+	
 	return version, err
 }
 
 // SecretExists Checks if secret exists
-func SecretExists(secretName string) bool {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	accessRequest := &secretmanagerpb.GetSecretRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v", ProjectID, secretName)}
-	_, err := client.GetSecret(ctx, accessRequest)
+func (c *Client) SecretExists(ctx context.Context, secretName string, projectId string) bool {
+	accessRequest := pb.GetSecretRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v", projectId, secretName)}
+	
+	_, err := c.smc.GetSecret(ctx, &accessRequest)
 	if err != nil {
 		return false
 	}
@@ -120,63 +121,62 @@ func SecretExists(secretName string) bool {
 }
 
 // AddNewSecretVersion Adds a new Version of a secret on a secret name
-func AddNewSecretVersion(secretName string, payload []byte) (*secretmanagerpb.SecretVersion, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	addSecretVersionReq := &secretmanagerpb.AddSecretVersionRequest{
-		Parent: fmt.Sprintf("projects/%v/secrets/%v", ProjectID, secretName),
-		Payload: &secretmanagerpb.SecretPayload{
+func (c *Client) AddNewSecretVersion(ctx context.Context, secretName string, projectId string, payload []byte) (*pb.SecretVersion, error) {
+	addSecretVersionReq := pb.AddSecretVersionRequest{
+		Parent: fmt.Sprintf("projects/%v/secrets/%v", projectId, secretName),
+		Payload: &pb.SecretPayload{
 			Data: payload,
 		},
 	}
-	version, err := client.AddSecretVersion(ctx, addSecretVersionReq)
+	version, err := c.smc.AddSecretVersion(ctx, &addSecretVersionReq)
 	if err != nil {
 		log.Printf("failed to add secret version: %v", err)
 		return nil, err
 	}
+	
 	return version, nil
 }
 
 // GetSecret Gets secret data
-func GetSecret(secretName string, version string) (*secretmanagerpb.SecretPayload, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+func (c *Client) GetSecret(ctx context.Context, secretName string, projectId string, version string) (*pb.SecretPayload, error) {
 	if version == "" {
 		version = "latest"
 	}
-	getSecret := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", ProjectID, secretName, version),
+	
+	getSecret := pb.AccessSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", projectId, secretName, version),
 	}
-	result, err := client.AccessSecretVersion(ctx, getSecret)
+	
+	result, err := c.smc.AccessSecretVersion(ctx, &getSecret)
 	if err != nil {
 		log.Printf("failed to get secret: %v", err)
 		return nil, err
 	}
+	
 	return result.Payload, nil
 }
 
 // DeleteSecretAndVersions Deletes secret with all the versions included
-func DeleteSecretAndVersions(secretName string) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	deleteSecretReq := &secretmanagerpb.DeleteSecretRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v", ProjectID, secretName),
+func (c *Client) DeleteSecretAndVersions(ctx context.Context, secretName string, projectId string) error {
+	deleteSecretReq := pb.DeleteSecretRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v", projectId, secretName),
 	}
-	err := client.DeleteSecret(ctx, deleteSecretReq)
+	
+	err := c.smc.DeleteSecret(ctx, &deleteSecretReq)
 	if err == nil {
 		log.Printf("Secret Deleted Successfully")
 	}
+	
 	return err
 }
 
 // DeleteSecretVersion Deletes specific version of a secret
-func DeleteSecretVersion(secretName string, version string) (*secretmanagerpb.SecretVersion, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	destroySecretReq := &secretmanagerpb.DestroySecretVersionRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", ProjectID, secretName, version),
+func (c *Client) DeleteSecretVersion(ctx context.Context, secretName string, projectId string, version string) (*pb.SecretVersion, error) {
+	
+	destroySecretReq := pb.DestroySecretVersionRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", projectId, secretName, version),
 	}
-	result, err := client.DestroySecretVersion(ctx, destroySecretReq)
+	result, err := c.smc.DestroySecretVersion(ctx, &destroySecretReq)
 	if err != nil {
 		log.Printf("failed to get secret: %v", err)
 		return nil, err
@@ -185,46 +185,46 @@ func DeleteSecretVersion(secretName string, version string) (*secretmanagerpb.Se
 }
 
 // GetSecretMetadata Gets metadata of a secret Name
-func GetSecretMetadata(secretName string, version string) (*secretmanagerpb.SecretVersion, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	getSecretReq := &secretmanagerpb.GetSecretVersionRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", ProjectID, secretName, version),
+func (c *Client) GetSecretMetadata(ctx context.Context, secretName string, projectId string, version string) (*pb.SecretVersion, error) {
+	getSecretReq := pb.GetSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", projectId, secretName, version),
 	}
-	result, err := client.GetSecretVersion(ctx, getSecretReq)
+	
+	result, err := c.smc.GetSecretVersion(ctx, &getSecretReq)
 	if err != nil {
 		log.Printf("failed to get secret: %v", err)
 		return nil, err
 	}
+	
 	return result, nil
 }
 
 // DisableSecret Disables secret
-func DisableSecret(secretName string, version string) (*secretmanagerpb.SecretVersion, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	disableSecretReq := &secretmanagerpb.DisableSecretVersionRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", ProjectID, secretName, version),
+func (c *Client) DisableSecret(ctx context.Context, secretName string, projectId string, version string) (*pb.SecretVersion, error) {
+	disableSecretReq := pb.DisableSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", projectId, secretName, version),
 	}
-	result, err := client.DisableSecretVersion(ctx, disableSecretReq)
+	
+	result, err := c.smc.DisableSecretVersion(ctx, &disableSecretReq)
 	if err != nil {
 		log.Printf("failed to get secret: %v", err)
 		return nil, err
 	}
+	
 	return result, nil
 }
 
 // EnableSecret Enables secret
-func EnableSecret(secretName string, version string) (*secretmanagerpb.SecretVersion, error) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	enableSecretReq := &secretmanagerpb.EnableSecretVersionRequest{
-		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", ProjectID, secretName, version),
+func (c *Client) EnableSecret(ctx context.Context, secretName string, projectId string, version string) (*pb.SecretVersion, error) {
+	enableSecretReq := pb.EnableSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%v/secrets/%v/versions/%v", projectId, secretName, version),
 	}
-	result, err := client.EnableSecretVersion(ctx, enableSecretReq)
+	
+	result, err := c.smc.EnableSecretVersion(ctx, &enableSecretReq)
 	if err != nil {
 		log.Printf("failed to get secret: %v\n", err)
 		return nil, err
 	}
+	
 	return result, nil
 }
